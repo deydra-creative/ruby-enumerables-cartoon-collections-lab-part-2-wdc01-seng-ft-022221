abce3e08738f3548e610ae9e2f0f5b8959e1893b
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 17:38:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,13 @@
 def square_array(array)
-  
+  new_array = []
+  index = 0
+ 
+  while index < array.length do
+    new_array << array[index] * array[index]
+    index += 1
+  end
+ 
+  new_array
 end
 
 def summon_captain_planet(planeteer_calls)
