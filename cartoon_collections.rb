e8018f2f1ca66792afd5ff4c46b2b0beccf282d8
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 17:43:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,7 @@ def long_planeteer_calls(planeteer_calls)
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  
+  planeteer_calls.find do |call|
+  valid_calls.include? call
+end
 end
