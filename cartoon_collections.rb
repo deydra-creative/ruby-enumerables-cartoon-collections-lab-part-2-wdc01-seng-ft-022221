43bea2f08cacdda8c9c87c358b8832b7f7faf494
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 17:43:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  
+ planeteer_calls.any? do |call|
+    call.length > 4 
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
