3af645a566734f846856c012f52833da7ad7f1f5
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 17:44:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,18 +3,23 @@ def square_array(array)
     element**2
 end
 
+
 def summon_captain_planet(planeteer_calls)
  planeteer_calls.map do |call|
     call.insert(-1,"!")
     call.capitalize
 end
 
+
 def long_planeteer_calls(planeteer_calls)
  planeteer_calls.any? do |call|
     call.length > 4 
+  end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |call|
   valid_calls.include? call
+end
+
 end
